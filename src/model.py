c216215786dd27d46cab6012e56ce1cd6340b6b0
kimjaegyeom <worua5667@inha.edu>
2024-09-04 15:12:37 +0900

    deberta_model

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from transformers import BertModel
+from transformers import AutoModelForSequenceClassification
 
 class BertForMultiLabelClassification(nn.Module):
     def __init__(self, num_labels):
@@ -16,3 +17,16 @@ class BertForMultiLabelClassification(nn.Module):
         pooled_output = outputs[1]
         pooled_output = self.dropout(pooled_output)
         return self.classifier(pooled_output)
+
+
+class DebertaForMultiLabelClassification(nn.Module):
+    def __init__(self, num_labels):
+        super(DebertaForMultiLabelClassification, self).__init__()
+        self.deberta = AutoModelForSequenceClassification.from_pretrained(
+            "mrm8488/deberta-v3-small-finetuned-cola", 
+            num_labels=num_labels
+        )
+        
+    def forward(self, input_ids, attention_mask):
+        outputs = self.deberta(input_ids=input_ids, attention_mask=attention_mask)
+        return outputs.logits
