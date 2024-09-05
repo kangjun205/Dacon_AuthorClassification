2297323aa13114dae17a98f6e3f666749883dca3
KyungJun <kangjun205@gmail.com>
2024-09-05 09:56:12 +0900

    feat: BERT 모델 output layer에 linear transform + relu 추가

--- src/model.py
+++ src/model.py
@@ -4,11 +4,13 @@ from transformers import AutoModel
 from transformers import AutoModelForSequenceClassification
 
 class BertForMultiLabelClassification(nn.Module):
-    def __init__(self, num_labels):
+    def __init__(self, num_labels, dim_hidden):
         super(BertForMultiLabelClassification, self).__init__()
         self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)
+        self.dense = nn.Linear(self.bert.config.hidden_size, dim_hidden)
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(dim_hidden, num_labels)
 
         for param in self.bert.parameters():
             param.requires_grad = False
@@ -17,7 +19,9 @@ class BertForMultiLabelClassification(nn.Module):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         pooled_output = outputs[1]
         pooled_output = self.dropout(pooled_output)
-        return self.classifier(pooled_output)
+        hidden_layer = self.dense(pooled_output)
+        hidden_layer = self.relu(hidden_layer)
+        return self.classifier(hidden_layer)
 
 class DebertaForMultiLabelClassification(nn.Module):
     def __init__(self, num_labels):
